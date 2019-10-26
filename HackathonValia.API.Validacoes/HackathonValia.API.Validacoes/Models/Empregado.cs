96acf5a1fa965cacb0730dcf907795f68e52061c
Luís Cláudio Ribeiro Junior <luis.claudio.jr@hotmail.com>
2019-10-26 11:46:17 -0300

    apiFuncionando

---
+++ HackathonValia.API.Validacoes/HackathonValia.API.Validacoes/Models/Empregado.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackathonValia.API.Validacoes.Models
+{
+    public class EmpregadoModel
+    {
+        
+        public string Grupo { get; set; }
+
+        
+        public string CodEmpresa { get; set; }
+
+        
+        public string Registro { get; set; }
+
+        
+        public string Nome { get; set; }
+
+        
+        public string CPF { get; set; }
+
+        
+        public string RG { get; set; }
+
+        
+        public string RGOrgaoExpedidor { get; set; }
+
+        
+        public string OrgaoLotaco { get; set; }
+
+        
+        public DateTime DataNascimento { get; set; }
+
+        
+        public string SituacaoPatrocinador { get; set; }
+
+        
+        public int CodEstadoCivil { get; set; }
+
+        
+        public string Pai { get; set; }
+
+        
+        public string Mae { get; set; }
+
+        
+        public string Sexo { get; set; }
+
+        
+        public string Endereco { get; set; }
+
+        
+        public string Bairro { get; set; }
+
+        
+        public string Cidade { get; set; }
+
+        
+        public string UF { get; set; }
+
+        
+        public string CEP { get; set; }
+
+        
+        public string DDDTelefone { get; set; }
+
+        
+        public string Telefone { get; set; }
+
+        
+        public DateTime DataInscricao { get; set; }
+
+        
+        public string Banco { get; set; }
+
+        
+        public string Agencia { get; set; }
+
+        
+        public string ContaCorrente { get; set; }
+
+        
+        public string Nacionalidade { get; set; }
+
+        
+        public string Ramal { get; set; }
+
+        
+        public string Salario { get; set; }
+
+        
+        public string Email { get; set; }
+
+        
+        public string Celular { get; set; }
+
+        
+        public string DDDCelular { get; set; }
+
+        
+        public DateTime DataAdmissao { get; set; }
+
+        
+        public DateTime DataAfastamento { get; set; }
+
+        
+        public DateTime DtInicioAlteracaoSituacao { get; set; }
+
+        
+        public DateTime DataDemissao { get; set; }
+
+        
+        public DateTime DataDesligamentoPlano { get; set; }
+
+        
+        public string CodMotivoDemissao { get; set; }
+
+        
+        public DateTime DataObito { get; set; }
+
+        
+        public int CodConfidencialidade { get; set; }
+
+        
+        public int CodContratoTrabalho { get; set; }
+
+        
+        public string CodEstrutural { get; set; }
+
+        
+        public DateTime RGDataExpedicao { get; set; }
+
+        
+        public string PercentualContribuicao { get; set; }
+
+        
+        public string Plano { get; set; }
+
+        
+        public string ContribuicaoEsporadica { get; set; }
+
+        
+        public DateTime DataInicio { get; set; }
+
+        
+        public DateTime DataFim { get; set; }
+
+        
+        public string Livre { get; set; }
+
+        
+        public string Tipo { get; set; }
+
+    }
+}
